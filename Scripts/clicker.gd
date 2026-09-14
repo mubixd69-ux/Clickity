551ac29ef83b7a8cf32c86d6de9f9e7382890d51
mubixd69-ux <mubixd69@gmail.com>
2026-09-14 19:41:48 +0300

    Added a breads per minute counter (doesnt work)

--- Scripts/clicker.gd
+++ Scripts/clicker.gd
@@ -2,6 +2,7 @@ extends Sprite2D
 
 @onready var sprite: Sprite2D = $"."
 @onready var score_label: RichTextLabel = $"../CanvasLayer/HUD/ScoreLabel"
+@onready var bpm_label: RichTextLabel = $"../CanvasLayer/HUD/BPM"
 
 var bounce: Tween
 var flash: Tween
@@ -13,7 +14,10 @@ var label_base_scale: Vector2 = Vector2.ONE
 
 var spawn_radius: float = 240.0
 
-	
+var bread_earned_this_second: int = 0
+var current_bpm: int = 0
+
+
 func update_score_ui() -> void:
 	if not score_label:
 		return
@@ -29,6 +33,15 @@ func update_score_ui() -> void:
 	score_label.scale = label_base_scale * 1.25
 	score = create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 	score.tween_property(score_label, "scale", label_base_scale, 0.2)
+	
+func update_bpm_ui() -> void:
+	if not bpm_label:
+		return
+		
+	
+		
+	
+	bpm_label.text = "[center][wave amp=30.0 freq=5.0][color=#FFFFFF]" + str(clicks) + " BPM[/color][/center]"
 
 func _ready() -> void:
 	base_scale = sprite.scale
@@ -37,6 +50,23 @@ func _ready() -> void:
 		score_label.pivot_offset = score_label.size / 2.0
 		label_base_scale = score_label.scale
 		update_score_ui()
+		update_bpm_ui()
+	
+	var bpm_timer := Timer.new()
+	bpm_timer.wait_time = 1.0
+	bpm_timer.autostart = true
+	bpm_timer.timeout.connect(bpm_timer_timeout)
+	add_child(bpm_timer)
+
+func bpm_timer_timeout() -> void:
+	current_bpm = bread_earned_this_second * 60
+	bread_earned_this_second = 0
+	update_bpm_ui()
+	
+func add_bread(amount: int) -> void:
+	clicks += amount
+	bread_earned_this_second += amount
+	update_bpm_ui()
 
 func _on_area_2d_mouse_entered() -> void:
 	print("1")
@@ -83,7 +113,7 @@ func spawn_label() -> void:
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
-			clicks += 1
+			add_bread(1)
 			print(clicks)
 			white()
 			animate()
