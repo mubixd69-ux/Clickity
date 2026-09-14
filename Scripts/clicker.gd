44d48c6a8c5345f75f015a1465e0880aca2c7970
mubixd69-ux <mubixd69@gmail.com>
2026-09-14 16:09:36 +0300

    Added a bread that you can click
    This is the main object that you click and I made it so the sprite flashes white whenever its pressed

--- Scripts/clicker.gd
+++ Scripts/clicker.gd
@@ -1,19 +1,24 @@
 extends Sprite2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+@onready var sprite: Sprite2D = $"."
+var flash: Tween
 
 func _on_area_2d_mouse_entered() -> void:
-	pass # Replace with function body.
+	print("1")
 
 
 func _on_area_2d_mouse_exited() -> void:
-	pass # Replace with function body.
+	print("2")
+
+func white():
+	if flash and flash.is_valid():
+		flash.kill()
+	sprite.modulate = Color(10, 10, 10)
+	flash = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	flash.tween_property(sprite, "modulate", Color(1, 1, 1),0.25)
+	
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			print(3)
+			white()
