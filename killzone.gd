ae420875dcf7e072c7c2297f3fdf3f0cd27a002c
theEerSOYEAH <85411618+theEerSOYEAH@users.noreply.github.com>
2026-09-20 15:13:05 +0300

    killzone improvement
    made the killzone makes u lose 20% of ur clicks when stepped on

--- killzone.gd
+++ killzone.gd
@@ -1,6 +1,22 @@
 extends Area2D
 @onready var sound = $AudioStreamPlayer2D2
+@onready var colission = $CollisionShape2D
+var is_dying: bool = false
 
 func _on_body_entered(body: CharacterBody2D) -> void:
-	if body.is_in_group("player"):
-		$AudioStreamPlayer2D.play()
+	if body.is_in_group("player") and not is_dying:
+		is_dying = true
+		
+		GlobalManager.clicks -= GlobalManager.clicks / 5
+		
+		Sidetransition.anim_player.speed_scale = 2.5
+		
+		Sidetransition.change_scene("res://Scenes/main.tscn")
+		
+		sound.play()
+		
+		await sound.finished
+		get_tree().paused = true
+		colission.set_deferred("disabled", true)
+		
+ 		
